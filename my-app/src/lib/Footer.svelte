6b5430d5ce34a2fb30ba77cb6bf1b80669e2c7ec
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-09-01 11:56:54 +1200

    footer completed, was a pain.

--- my-app/src/lib/Footer.svelte
+++ my-app/src/lib/Footer.svelte
@@ -1,29 +1,114 @@
 <div class="footer">
-    <footer>
+           
         <div class = "contactInfo">
-            <h1>
-                Contact Info
-            </h1>
-            <p>heavenlyvision@gmail.com</p>
-            <p>PH: 03 789 97490</p>
+            <div class="top">
+               <h2>
+                    Contact Info
+                </h2> 
+            </div>
+            <div class="bottom">
+                 <ul>
+                    <li>heavenlyvision@gmail.com</li>
+                    <li>PH: 03 789 97490</li>
+                 </ul>
+                
+            </div>
+           
         </div> 
         <div class="about">
-            <h1>
-                About
-            </h1>
-            <p>Gallary</p>
-            <p>The Tours</p>
+            <div class = "top">
+                 <h2>
+                    About
+                </h2>
+            </div>
+           <div class = "bottom">
+            <ul>
+                <li>Gallery</li>
+                <li>The Tours</li>
+            </ul>
+           </div>
         </div>       
         <div class= "services">
-            <h1>
-                Services
-            </h1>
-            <p>Equipment</p>
-            <p>Bus ride</p>
-            <p>Locations</p>
+            <div class= "top">
+                <h2>
+                    Services
+                </h2>
+            </div>
+            <div class ="bottom">
+                <ul>
+                    <li>Equipment</li>
+                    <li>Bus ride</li>
+                    <li>Locations</li>
+                </ul>              
+            </div>
+           
         </div>      
-    </footer>
+    
 </div>
+
+<style>
+.footer{
+    background-color: rgb(168,193,214);
+    font-family:  'Poppins', sans-serif;
+    font-size: small;    
+    height: 150px;   
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+   justify-content: space-evenly;
+    align-items: center;
+    margin: 0;
+}
+.contactInfo{
+    
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    
+}
+.contactInfo h4{
+    display: table-cell;
+    vertical-align: top;
+}
+
+.about{
+    
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+}
+    
+
+.services{
+   height: 100%;
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+}
+
+li{
+    list-style: none;
+}
+.top{
+    height: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+.bottom{
+    height: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+    
+
+
+
+
+</style>
+
 
 
 
