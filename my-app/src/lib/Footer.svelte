4c8a50fa7630f445ac196b1990849e7640531072
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-09-01 10:24:01 +1200

    About section added

--- my-app/src/lib/Footer.svelte
+++ my-app/src/lib/Footer.svelte
@@ -6,6 +6,13 @@
             </h1>
             <p>heavenlyvision@gmail.com</p>
             <p>PH: 03 789 97490</p>
+        </div> 
+        <div class="about">
+            <h1>
+                About
+            </h1>
+            <p>Gallary</p>
+            <p>The Tours</p>
         </div>       
     </footer>
 </div>
