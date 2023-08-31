6923a77ba044a312617e28fce543ab0cbf5939f7
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-09-01 09:53:27 +1200

    I have started on gallery by adding some div's

---
+++ my-app/src/lib/Gallery.svelte
@@ -0,0 +1,19 @@
+<div class="outer">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+
+
+<style>
+
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        
+    }
+    .outer{
+        width: 100%;
+    }
+
+</style>
